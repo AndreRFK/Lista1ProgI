01f2f7f73739166871d75e3329e7489472b260d2
alunoadm <alunoadm@MSCGRESL04016.estacio.acad>
2017-09-27 22:51:05 -0300

    Lista de exercicio 1

--- JavaApplication3/src/br/estacio/lista1/Exercicio12.java
+++ JavaApplication3/src/br/estacio/lista1/Exercicio12.java
@@ -4,30 +4,26 @@
  * and open the template in the editor.
  */
 package br.estacio.lista1;
-
 import java.util.Scanner;
 
 /**
  *
  * @author alunoadm
  */
-public class Exercicio11 {
+public class Exercicio12 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner numeroDigitado = new Scanner(System.in);
-        System.out.println("Digite um numero: ");
-        int i = numeroDigitado.nextInt();
-        if (i < 10) {
-            System.out.println(i + " É menor que 10");
-        } else if (i > 10){
-            System.out.println(i + " É maior que 10");
-        } else if (i == 10){
-            System.out.println(i + " É igual a 10");
-        }
+        int x;
+        int y;
+        int z;
+        Scanner xT = new Scanner (System.in);
+        Scanner yT = new Scanner (System.in);
+        Scanner zT = new Scanner (System.in);
+        
     }
-
+    
 }
